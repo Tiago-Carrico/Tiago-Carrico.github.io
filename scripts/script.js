df51740ab80baed989962b88043e48f5cfe5b233
Tiago-Carrico <49901138+Tiago-Carrico@users.noreply.github.com>
2023-11-21 23:13:35 +0000

    cats facts working, plus stuff on html to make it better and more clear

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  getCatFact();
+  getCatImg();
   var checkboxTheme = document.querySelector('.container_toggle');
   document.documentElement.setAttribute('data-theme', 'light');
+
   checkboxTheme.addEventListener('change', function() {
 
     //console.log(document.documentElement.getAttribute('data-theme')); //working but starts at null??
@@ -24,3 +27,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 1000)
   }
 })
+
+//https://codepen.io/tlcheung/pen/YzqGXPK
+function getCatFact() {
+  let catFact = "";
+  let http = new XMLHttpRequest();
+  http.open("GET", "https://catfact.ninja/fact", true);
+  http.send();
+
+  http.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let response = this.responseText;
+      let jsonResponse = JSON.parse(response);
+      catFact = jsonResponse.fact;
+      document.getElementById("cat-fact").innerHTML = catFact;
+    }
+  }
+}
+
+/*https://codepen.io/thebigdavec/pen/eYWNemj */
+async function getCatImg(){
+  const img = document.getElementById("random-cat");
+  const catURL = await fetch("https://api.thecatapi.com/v1/images/search");
+  const catData = await catURL.json();
+  const catImg = catData[0].url;
+  img.src = catImg;
+}
